061f6ccf368eebae703a7feca2db52dff2eab2e4
Alex Forencich <alex@alexforencich.com>
2012-10-20 00:35:39 -0700

    Incremental commit of AWG2000 series driver

--- pyivi/tektronix/tektronixAWG2000.py
+++ pyivi/tektronix/tektronixAWG2000.py
@@ -24,10 +24,23 @@ THE SOFTWARE.
 
 """
 
+import time
+
 from .. import ivi
 from .. import fgen
 
-class tektronixAWG2000(ivi.Driver, fgen.Base):
+StandardWaveformMapping = {
+        'sine': 'sin',
+        'square': 'squ',
+        'triangle': 'tri',
+        'ramp_up': 'ramp',
+        #'ramp_down',
+        #'dc'
+        }
+
+class tektronixAWG2000(ivi.Driver, fgen.Base, fgen.StdFunc, fgen.ArbWfm,
+                fgen.ArbFrequency, fgen.ArbSeq, fgen.SoftwareTrigger,
+                fgen.Burst, fgen.ArbChannelWfm):
     "Tektronix AWG2000 series arbitrary waveform generator driver"
     
     def __init__(self, *args, **kwargs):
@@ -36,6 +49,17 @@ class tektronixAWG2000(ivi.Driver, fgen.Base):
         self._instrument_id = 'AWG2000'
         
         self._output_count = 1
+        
+        self._arbitrary_sample_rate = 0
+        self._arbitrary_waveform_number_waveforms_max = 0
+        self._arbitrary_waveform_size_max = 0
+        self._arbitrary_waveform_size_min = 0
+        self._arbitrary_waveform_quantum = 0
+        
+        self._arbitrary_sequence_number_sequences_max = 0
+        self._arbitrary_sequence_loop_count_max = 0
+        self._arbitrary_sequence_length_max = 0
+        self._arbitrary_sequence_length_min = 0
         
         self._identity_description = "Tektronix AWG2000 series arbitrary waveform generator driver"
         self._identity_identifier = ""
@@ -135,7 +159,10 @@ class tektronixAWG2000(ivi.Driver, fgen.Base):
         code = 0
         message = "Self test passed"
         if not self._driver_operation_simulate:
-            code = int(self._ask("*TST?"))
+            self._write("*TST?")
+            # wait for test to complete
+            time.sleep(60)
+            code = int(self._read())
             if code != 0:
                 message = "Self test failed"
         return (code, message)
@@ -151,6 +178,287 @@ class tektronixAWG2000(ivi.Driver, fgen.Base):
         self._output_enabled = list()
         for i in range(self._output_count):
             self._output_enabled.append(False)
+    
+    
+    
+    def _get_output_operation_mode(self, index):
+        index = ivi.get_index(self._output_name, index)
+        return self._output_operation_mode[index]
+    
+    def _set_output_operation_mode(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        if value not in OperationMode:
+            raise ivi.ValueNotSupportedException()
+        self._output_operation_mode[index] = value
+    
+    def _get_output_enabled(self, index):
+        index = ivi.get_index(self._output_name, index)
+        if not self._driver_operation_simulate and not self._get_cache_valid(index=index):
+            resp = self._ask(":output:ch%d:state?" % (index+1)).split(' ')[1]
+            self._output_standard_waveform_amplitude[index] = bool(int(resp))
+            self._set_cache_valid(index=index)
+        return self._output_enabled[index]
+    
+    def _set_output_enabled(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = bool(value)
+        if not self._driver_operation_simulate:
+            self._write(":output:ch%d:state %d" % (index+1, value))
+        self._output_enabled[index] = value
+        self._set_cache_valid(index=index)
+    
+    def _get_output_impedance(self, index):
+        index = ivi.get_index(self._output_name, index)
+        self._output_impedance[index] = 50
+        return self._output_impedance[index]
+    
+    def _set_output_impedance(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = 50
+        self._output_impedance[index] = value
+    
+    def _get_output_mode(self, index):
+        index = ivi.get_index(self._output_name, index)
+        if not self._driver_operation_simulate and not self._get_cache_valid(index=index):
+            resp = self._ask(":fg:state?").split(' ')[1]
+            if int(resp):
+                self._output_mode[index] = 'function'
+            else:
+                self._output_mode[index] = 'arbitrary'
+            self._set_cache_valid(index=index)
+        return self._output_mode[index]
+    
+    def _set_output_mode(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        if value not in fgen.OutputMode:
+            raise ivi.ValueNotSupportedException()
+        if not self._driver_operation_simulate:
+            if value == 'function':
+                self._write(":fg:state 1")
+            elif value == 'arbitrary':
+                self._write(":fg:state 0")
+        self._output_mode[index] = value
+        for k in range(self._output_count):
+            self._set_cache_valid(valid=False,index=k)
+        self._set_cache_valid(index=index)
+    
+    def _get_output_reference_clock_source(self, index):
+        index = ivi.get_index(self._output_name, index)
+        return self._output_reference_clock_source[index]
+    
+    def _set_output_reference_clock_source(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = str(value)
+        self._output_reference_clock_source[index] = value
+    
+    def abort_generation(self):
+        pass
+    
+    def initiate_generation(self):
+        pass
+    
+    def _get_output_standard_waveform_amplitude(self, index):
+        index = ivi.get_index(self._output_name, index)
+        if not self._driver_operation_simulate and not self._get_cache_valid(index=index):
+            resp = self._ask(":fg:ch%d:amplitude?" % (index+1)).split(' ')[1]
+            self._output_standard_waveform_amplitude[index] = float(resp)
+            self._set_cache_valid(index=index)
+        return self._output_standard_waveform_amplitude[index]
+    
+    def _set_output_standard_waveform_amplitude(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = float(value)
+        if not self._driver_operation_simulate:
+            self._write(":fg:ch%d:amplitude %e" % (index+1, value))
+        self._output_standard_waveform_amplitude[index] = value
+        self._set_cache_valid(index=index)
+    
+    def _get_output_standard_waveform_dc_offset(self, index):
+        index = ivi.get_index(self._output_name, index)
+        if not self._driver_operation_simulate and not self._get_cache_valid(index=index):
+            resp = self._ask(":fg:ch%d:offset?" % (index+1)).split(' ')[1]
+            self._output_standard_waveform_dc_offset[index] = float(resp)
+            self._set_cache_valid(index=index)
+        return self._output_standard_waveform_dc_offset[index]
+    
+    def _set_output_standard_waveform_dc_offset(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = float(value)
+        if not self._driver_operation_simulate:
+            self._write(":fg:ch%d:offset %e" % (index+1, value))
+        self._output_standard_waveform_dc_offset[index] = value
+        self._set_cache_valid(index=index)
+    
+    def _get_output_standard_waveform_duty_cycle_high(self, index):
+        index = ivi.get_index(self._output_name, index)
+        return self._output_standard_waveform_duty_cycle_high[index]
+    
+    def _set_output_standard_waveform_duty_cycle_high(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = float(value)
+        self._output_standard_waveform_duty_cycle_high[index] = value
+    
+    def _get_output_standard_waveform_start_phase(self, index):
+        index = ivi.get_index(self._output_name, index)
+        return self._output_standard_waveform_start_phase[index]
+    
+    def _set_output_standard_waveform_start_phase(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = float(value)
+        self._output_standard_waveform_start_phase[index] = value
+    
+    def _get_output_standard_waveform_frequency(self, index):
+        index = ivi.get_index(self._output_name, index)
+        if not self._driver_operation_simulate and not self._get_cache_valid(index=index):
+            resp = self._ask(":fg:frequency?").split(' ')[1]
+            self._output_standard_waveform_frequency[index] = float(resp)
+            self._set_cache_valid(index=index)
+        return self._output_standard_waveform_frequency[index]
+    
+    def _set_output_standard_waveform_frequency(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = float(value)
+        if not self._driver_operation_simulate:
+            self._write(":fg:frequency %e" % value)
+        self._output_standard_waveform_frequency[index] = value
+        for k in range(self._output_count):
+            self._set_cache_valid(valid=False,index=k)
+        self._set_cache_valid(index=index)
+    
+    def _get_output_standard_waveform_waveform_function(self, index):
+        index = ivi.get_index(self._output_name, index)
+        if not self._driver_operation_simulate and not self._get_cache_valid(index=index):
+            resp = self._ask(":fg:ch%d:shape?" % (index+1)).split(' ')[1]
+            value = resp.lower()
+            value = [k for k,v in StandardWaveformMapping.items() if v==value][0]
+            self._output_standard_waveform_waveform_function[index] = value
+            self._set_cache_valid(index=index)
+        return self._output_standard_waveform_waveform_function[index]
+    
+    def _set_output_standard_waveform_waveform_function(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        if value not in StandardWaveformMapping:
+            raise ivi.ValueNotSupportedException()
+        if not self._driver_operation_simulate:
+            self._write(":fg:ch%d:shape %s" % (index+1, StandardWaveformMapping[value]))
+        self._output_standard_waveform_waveform_function[index] = value
+        self._set_cache_valid(index=index)
+    
+    def _get_output_arbitrary_gain(self, index):
+        index = ivi.get_index(self._output_name, index)
+        if not self._driver_operation_simulate and not self._get_cache_valid(index=index):
+            resp = self._ask(":ch%d:amplitude?" % (index+1)).split(' ')[1]
+            self._output_arbitrary_gain[index] = float(resp)
+            self._set_cache_valid(index=index)
+        return self._output_arbitrary_gain[index]
+    
+    def _set_output_arbitrary_gain(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = float(value)
+        if not self._driver_operation_simulate:
+            self._write(":ch%d:amplitude %e" % (index+1, value))
+        self._output_arbitrary_gain[index] = value
+        self._set_cache_valid(index=index)
+    
+    def _get_output_arbitrary_offset(self, index):
+        index = ivi.get_index(self._output_name, index)
+        if not self._driver_operation_simulate and not self._get_cache_valid(index=index):
+            resp = self._ask(":ch%d:offset?" % (index+1)).split(' ')[1]
+            self._output_arbitrary_offset[index] = float(resp)
+            self._set_cache_valid(index=index)
+        return self._output_arbitrary_offset[index]
+    
+    def _set_output_arbitrary_offset(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = float(value)
+        if not self._driver_operation_simulate:
+            self._write(":ch%d:offset %e" % (index+1, value))
+        self._output_arbitrary_offset[index] = value
+        self._set_cache_valid(index=index)
+    
+    def _get_output_arbitrary_waveform(self, index):
+        index = ivi.get_index(self._output_name, index)
+        return self._output_arbitrary_waveform[index]
+    
+    def _set_output_arbitrary_waveform(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = str(value)
+        self._output_arbitrary_waveform[index] = value
+    
+    def _get_arbitrary_sample_rate(self):
+        return self._arbitrary_sample_rate
+    
+    def _set_arbitrary_sample_rate(self, value):
+        value = float(value)
+        self._arbitrary_sample_rate = value
+    
+    def _get_arbitrary_waveform_number_waveforms_max(self):
+        return self._arbitrary_waveform_number_waveforms_max
+    
+    def _get_arbitrary_waveform_size_max(self):
+        return self._arbitrary_waveform_size_max
+    
+    def _get_arbitrary_waveform_size_min(self):
+        return self._arbitrary_waveform_size_min
+    
+    def _get_arbitrary_waveform_quantum(self):
+        return self._arbitrary_waveform_quantum
+    
+    def _arbitrary_waveform_clear(self, handle):
+        pass
+    
+    def _arbitrary_waveform_create(self, data):
+        return "handle"
+    
+    def _get_output_arbitrary_frequency(self, index):
+        index = ivi.get_index(self._output_name, index)
+        return self._output_arbitrary_frequency[index]
+    
+    def _set_output_arbitrary_frequency(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = float(value)
+        self._output_arbitrary_frequency[index] = value
+    
+    def _get_arbitrary_sequence_number_sequences_max(self):
+        return self._arbitrary_sequence_number_sequences_max
+    
+    def _get_arbitrary_sequence_loop_count_max(self):
+        return self._arbitrary_sequence_loop_count_max
+    
+    def _get_arbitrary_sequence_length_max(self):
+        return self._arbitrary_sequence_length_max
+    
+    def _get_arbitrary_sequence_length_min(self):
+        return self._arbitrary_sequence_length_min
+    
+    def _arbitrary_clear_memory(self):
+        pass
+    
+    def _arbitrary_sequence_clear(self, handle):
+        pass
+    
+    def _arbitrary_sequence_configure(self, index, handle, gain, offset):
+        pass
+    
+    def _arbitrary_sequence_create(self, handle_list, loop_count_list):
+        return "handle"
+    
+    def send_software_trigger(self):
+        if not self._driver_operation_simulate:
+            self._write("*TRG")
+    
+    def _get_output_burst_count(self, index):
+        index = ivi.get_index(self._output_name, index)
+        return self._output_burst_count[index]
+    
+    def _set_output_burst_count(self, index, value):
+        index = ivi.get_index(self._output_name, index)
+        value = int(value)
+        self._output_burst_count[index] = value
+    
+    def _arbitrary_waveform_create_channel_waveform(self, index, data):
+        index = ivi.get_index(self._output_name, index)
+        return 'handle'
     
     
 
