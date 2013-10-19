bcd81815dcdf1df594fb5881d2010a307adcb8ae
Alex Forencich <alex@alexforencich.com>
2013-10-18 19:37:01 -0700

    Add generic SCPI power supply driver

--- ivi/scpi/__init__.py
+++ ivi/scpi/__init__.py
@@ -25,15 +25,8 @@ THE SOFTWARE.
 """
 
 __all__ = [
-        # Base IVI class
-        "ivi",
-        # IVI abstract classes
-        "scope", "dmm", "fgen", "dcpwr", "pwrmeter", "rfsiggen", "specan",
-        # Generic IVI drivers
-        "scpi",
-        # IVI drivers
-        "agilent", "tektronix"]
+        # IVI implementations
+        "dcpwr"]
 
-from .ivi import *
 from . import *
 
