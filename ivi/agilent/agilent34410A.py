cc2acb5c71735660aa47bb86a937fcbb670190ab
Alex Forencich <alex@alexforencich.com>
2013-10-20 02:35:40 -0700

    Convert Agilent 34410A driver to use SCPI generic driver

--- ivi/agilent/agilent34410A.py
+++ ivi/agilent/agilent34410A.py
@@ -29,24 +29,9 @@ import struct
 
 from .. import ivi
 from .. import dmm
+from .. import scpi
 
-MeasurementFunctionMapping = {
-        'dc_volts': 'volt',
-        'ac_volts': 'volt:ac',
-        'dc_current': 'curr',
-        'ac_current': 'curr:ac',
-        'two_wire_resistance': 'res',
-        'four_wire_resistance': 'fres',
-        #'ac_plus_dc_volts': 'volt',
-        #'ac_plus_dc_current': 'curr',
-        'frequency': 'freq',
-        'period': 'per',
-        'temperature': 'temp',
-        'capacitance': 'cap',
-        'continuity': 'cont',
-        'diode': 'diod'}
-
-class agilent34410A(ivi.Driver, dmm.Base):
+class agilent34410A(scpi.dmm.Base):
     "Agilent 34410A IVI DMM driver"
     
     def __init__(self, *args, **kwargs):
@@ -65,7 +50,7 @@ class agilent34410A(ivi.Driver, dmm.Base):
         self._identity_instrument_firmware_revision = ""
         self._identity_specification_major_version = 4
         self._identity_specification_minor_version = 1
-        self._identity_supported_instrument_models =['34410A', '34411A']
+        self._identity_supported_instrument_models = ['34410A', '34411A']
         
         ivi.add_method(self, 'memory.save',
                         self._memory_save)
@@ -98,161 +83,7 @@ class agilent34410A(ivi.Driver, dmm.Base):
             self.utility.reset()
         
     
-    def _load_id_string(self):
-        if self._driver_operation_simulate:
-            self._identity_instrument_manufacturer = "Not available while simulating"
-            self._identity_instrument_model = "Not available while simulating"
-            self._identity_instrument_firmware_revision = "Not available while simulating"
-        else:
-            lst = self._ask("*IDN?").split(",")
-            self._identity_instrument_manufacturer = lst[0]
-            self._identity_instrument_model = lst[1]
-            self._identity_instrument_firmware_revision = lst[3]
-            self._set_cache_valid(True, 'identity_instrument_manufacturer')
-            self._set_cache_valid(True, 'identity_instrument_model')
-            self._set_cache_valid(True, 'identity_instrument_firmware_revision')
     
-    def _get_identity_instrument_manufacturer(self):
-        if self._get_cache_valid():
-            return self._identity_instrument_manufacturer
-        self._load_id_string()
-        return self._identity_instrument_manufacturer
-    
-    def _get_identity_instrument_model(self):
-        if self._get_cache_valid():
-            return self._identity_instrument_model
-        self._load_id_string()
-        return self._identity_instrument_model
-    
-    def _get_identity_instrument_firmware_revision(self):
-        if self._get_cache_valid():
-            return self._identity_instrument_firmware_revision
-        self._load_id_string()
-        return self._identity_instrument_firmware_revision
-    
-    def _utility_disable(self):
-        pass
-    
-    def _utility_error_query(self):
-        error_code = 0
-        error_message = "No error"
-        if not self._driver_operation_simulate:
-            error_code, error_message = self._ask(":system:error?").split(',')
-            error_code = int(error_code)
-            error_message = error_message.strip(' "')
-        return (error_code, error_message)
-    
-    def _utility_lock_object(self):
-        pass
-    
-    def _utility_reset(self):
-        if not self._driver_operation_simulate:
-            self._write("*RST")
-            self.driver_operation.invalidate_all_attributes()
-    
-    def _utility_reset_with_defaults(self):
-        self._utility_reset()
-    
-    def _utility_self_test(self):
-        code = 0
-        message = "Self test passed"
-        if not self._driver_operation_simulate:
-            self._write("*TST?")
-            # wait for test to complete
-            time.sleep(40)
-            code = int(self._read())
-            if code != 0:
-                message = "Self test failed"
-        return (code, message)
-    
-    def _utility_unlock_object(self):
-        pass
-    
-    def _get_measurement_function(self):
-        if not self._driver_operation_simulate and not self._get_cache_valid():
-            value = self._ask(":sense:function?").lower()
-            value = [k for k,v in TriggerTypeMapping.items() if v==value][0]
-            self._measurement_function = value
-            self._set_cache_valid()
-        return self._measurement_function
-    
-    def _set_measurement_function(self, value):
-        if value not in MeasurementFunctionMapping:
-            raise ivi.ValueNotSupportedException()
-        if not self._driver_operation_simulate:
-            self._write(":sense:function '%s'" % MeasurementFunctionMapping[value])
-        self._measurement_function = value
-        self._set_cache_valid()
-    
-    def _get_range(self):
-        return self._range
-    
-    def _set_range(self, value):
-        value = float(value)
-        self._range = value
-    
-    def _get_auto_range(self):
-        return self._auto_range
-    
-    def _set_auto_range(self, value):
-        if value not in Auto:
-            raise ivi.ValueNotSupportedException()
-        self._auto_range = value
-    
-    def _get_resolution(self):
-        return self._resolution
-    
-    def _set_resolution(self, value):
-        value = float(value)
-        self._resolution = value
-    
-    def _get_trigger_delay(self):
-        return self._trigger_delay
-    
-    def _set_trigger_delay(self, value):
-        value = float(value)
-        self._trigger_delay = value
-    
-    def _get_trigger_delay_auto(self):
-        return self._trigger_delay_auto
-    
-    def _set_trigger_delay_auto(self, value):
-        value = bool(value)
-        self._trigger_delay_auto = value
-    
-    def _get_trigger_source(self):
-        return self._trigger_source
-    
-    def _set_trigger_source(self, value):
-        value = str(value)
-        self._trigger_source = value
-    
-    def _measurement_abort(self):
-        if not self._driver_operation_simulate:
-            self._write(":abort")
-    
-    def _measurement_fetch(self, max_time):
-        if not self._driver_operation_simulate:
-            return float(self._ask(":fetch?"))
-        return 0.0
-    
-    def _measurement_initiate(self):
-        if not self._driver_operation_simulate:
-            self._write(":initiate")
-    
-    def _measurement_is_out_of_range(self, value):
-        return self._measurement_is_over_range(value) or self._measurement_is_under_range(value)
-    
-    def _measurement_is_over_range(self, value):
-        return False
-    
-    def _measurement_is_under_range(self, value):
-        return False
-    
-    def _measurement_read(self, max_time):
-        if not self._driver_operation_simulate:
-            return float(self._ask(":read?"))
-        return 0.0
     
     def _memory_save(self, index):
         index = int(index)
