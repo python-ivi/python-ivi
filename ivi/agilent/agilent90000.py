877fb86186c6397f754af33cbb74e955ca98447b
Alex Forencich <alex@alexforencich.com>
2014-01-22 21:03:13 -0800

    Fix fetch_waveform on Agilent Infiniium 90000 series

--- ivi/agilent/agilent90000.py
+++ ivi/agilent/agilent90000.py
@@ -138,6 +138,67 @@ class agilent90000(agilentBaseScope):
         self._channel_differential_skew[index] = value
         self._set_cache_valid(index=index)
     
+    def _measurement_fetch_waveform(self, index):
+        index = ivi.get_index(self._channel_name, index)
+        
+        if self._driver_operation_simulate:
+            return list()
+        
+        self._write(":waveform:byteorder msbfirst")
+        self._write(":waveform:unsigned 1")
+        self._write(":waveform:format word")
+        self._write(":waveform:points normal")
+        self._write(":waveform:source %s" % self._channel_name[index])
+        
+        # Read preamble
+        
+        pre = self._ask(":waveform:preamble?").split(',')
+        
+        format = int(pre[0])
+        type = int(pre[1])
+        points = int(pre[2])
+        count = int(pre[3])
+        xincrement = float(pre[4])
+        xorigin = float(pre[5])
+        xreference = int(float(pre[6]))
+        yincrement = float(pre[7])
+        yorigin = float(pre[8])
+        yreference = int(float(pre[9]))
+        
+        if type == 1:
+            raise scope.InvalidAcquisitionTypeException()
+        
+        if format != 2:
+            raise UnexpectedResponseException()
+        
+        self._write(":waveform:data?")
+        
+        # Read waveform data
+        raw_data = self._read_raw()
+        raw_data = ivi.decode_ieee_block(raw_data)
+        
+        # Split out points and convert to time and voltage pairs
+        
+        data = list()
+        for i in range(points):
+            x = ((i - xreference) * xincrement) + xorigin
+            
+            yval = struct.unpack(">H", raw_data[i*2:i*2+2])[0]
+            y = ((yval - yreference) * yincrement) + yorigin
+            
+            data.append((x, y))
+        
+        return data
+    
+    def _measurement_read_waveform(self, index, maximum_time):
+        return self._measurement_fetch_waveform(index)
+    
+    def _measurement_initiate(self):
+        if not self._driver_operation_simulate:
+            self._write(":acquire:complete 100")
+            self._write(":digitize")
+            self._set_cache_valid(False, 'trigger_continuous')
+    
     
     
 
