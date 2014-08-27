12954d2e171dd17cbfee42c987b1fda999438ec9
Christopher Johnson <chrisj.egr@gmail.com>
2014-08-27 18:04:09 +0800

    Commit LeCroy WaveRunner XiA driver, first pass

---
+++ ivi/lecroy/__init__.py
@@ -0,0 +1,37 @@
+"""
+
+Python Interchangeable Virtual Instrument Library
+
+Copyright (c) 2012 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
+# Oscilloscopes
+# WaveRunner Xi-A / MXi-A Oscilloscopes
+from .lecroyWR204MXIA import lecroyWR204MXIA
+from .lecroyWR204XIA import lecroyWR204XIA
+from .lecroyWR104MXIA import lecroyWR104MXIA
+from .lecroyWR104XIA import lecroyWR104XIA
+from .lecroyWR64MXIA import lecroyWR64MXIA
+from .lecroyWR64XIA import lecroyWR64XIA
+from .lecroyWR62XIA import lecroyWR62XIA
+from .lecroyWR44MXIA import lecroyWR44MXIA
+from .lecroyWR44XIA import lecroyWR44XIA
