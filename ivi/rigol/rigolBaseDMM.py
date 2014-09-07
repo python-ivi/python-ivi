3baf69224cb1effedc99d017d93e50cfe81fd110
sacherjj <sacherjj@gmail.com>
2014-09-07 14:52:42 -0400

    Initial Commit

---
+++ ivi/rigol/rigolBaseDMM.py
@@ -0,0 +1,73 @@
+"""
+
+Python Interchangeable Virtual Instrument Library
+
+Copyright (c) 2013-2014 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
+from .. import ivi
+from .. import dmm
+from .. import scpi
+
+class rigolBaseDMM(scpi.dmm.Base):
+    "Rigol DMM Base"
+
+    def __init__(self, *args, **kwargs):
+        self.__dict__.setdefault('_instrument_id', '')
+
+        super(rigolBaseDMM, self).__init__(*args, **kwargs)
+
+        self._memory_size = 5  #?
+
+        self._identity_supported_instrument_models = ['34410A', '34411A']
+        self._identity_description = "Rigol generic IVI DMM driver"
+        self._identity_identifier = ""
+        self._identity_revision = ""
+        self._identity_vendor = ""
+        self._identity_instrument_manufacturer = "Rigol Technologies"
+        self._identity_instrument_model = ""
+        self._identity_instrument_firmware_revision = ""
+        self._identity_specification_major_version = 1
+        self._identity_specification_minor_version = 0
+        self._identity_supported_instrument_models = ['DM3058', 'DM3058E', 'DM3068']
+
+    def _initialize(self, resource = None, id_query = False, reset = False, **keywargs):
+        "Opens an I/O session to the instrument."
+
+        super(rigolBaseDMM, self)._initialize(resource, id_query, reset, **keywargs)
+
+        # interface clear
+        if not self._driver_operation_simulate:
+            self._clear()
+
+        # reset
+        if reset:
+            self.utility.reset()
+
+
+if __name__ == '__main__':
+    # TODO - Add testing script
+    pass
+
+
+
+
