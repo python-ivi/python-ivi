efb0a66c9a709fd9424beaf24c4ae1e3faeb84c2
Baiju Alexander <baiju.alexander@acconeer.com>
2018-01-11 16:42:03 +0100

    Add support for Rohde Schwarz RF signal generator SMC100A

--- ivi/rohdeschwarz/rohdeschwarzSMC100A.py
+++ ivi/rohdeschwarz/rohdeschwarzSMC100A.py
@@ -1,10 +1,9 @@
+# coding=utf-8
 """
 
 Python Interchangeable Virtual Instrument Library
 
-Copyright (c) 2012-2017 Alex Forencich
-
-Modified by Jeff Wurzbach 2014
+Copyright (c) 2018 Baiju Alexander
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,36 +25,25 @@ THE SOFTWARE.
 
 """
 
-__all__ = [
-        # Base IVI class
-        "ivi",
-        # IVI abstract classes
-        "scope",
-        "dmm",
-        "fgen",
-        "dcpwr",
-        "swtch",
-        "pwrmeter",
-        "specan",
-        "rfsiggen",
-        "counter",
-        # Extra IVI base classes
-        "extra",
-        # Generic IVI drivers
-        "scpi",
-        # IVI drivers
-        "agilent",
-        "dicon",
-        "chroma",
-        "colby",
-        "ics",
-        "jdsu",
-        "lecroy",
-        "rigol",
-        "rohdeschwarz",
-        "tektronix",
-        "testequity"]
+from .rohdeschwarzBaseRFSigGen import *
 
-from .ivi import *
-from . import *
 
+class rohdeschwarzSMC100A(rohdeschwarzBaseRFSigGen):
+    "Rohde&Schwarz SMC 100A RF Signal Generator"
+    
+    def __init__(self, *args, **kwargs):
+        self.__dict__.setdefault('_instrument_id', 'SMC100A')
+        
+        super(rohdeschwarzSMC100A, self).__init__(*args, **kwargs)
+
+        # frequency limit in Hertz
+        self._frequency_low = 9e3
+        self._frequency_high = 1100e6        
+        # rf level limit in dBm
+        self._rf_level_low = -120.0
+        self._rf_level_high = 19
+        # rf rms voltage level limit in Volt
+        self._rf_rms_voltage_level_low = 223.61e-9
+        self._rf_rms_voltage_level_high = 1.993
+
+        self._initialize(*args, **kwargs)
